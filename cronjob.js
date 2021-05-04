308581ef8b4531e4697ca57cf89d29aad08db250
Karan Asthana <karan@p3infotech.in>
2021-05-04 21:53:48 +0530

    changed api function

--- cronjob.js
+++ cronjob.js
@@ -1,29 +1,45 @@
 const https = require('https');
+const moment = require('moment');
 
-const calendarByDistrictUrl = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=664&date=04-05-2021';
+const requirementData = require('./cronScheduleRequirements.js');
+
+const calendarByDistrictUrl = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?';
 
 const cronjobFunc = () => {
     console.log('running a task every minute');
-    apiCall();
+    apiCalls();
 };
 
-const apiCall = () => {
-    https.get(calendarByDistrictUrl, (resp) => {
-        let data = '';
+const apiCalls = () => {
+    const requirementObject = requirementData.requirementData;
+    console.log('api calls start');
+    console.log(JSON.stringify(requirementObject));
+
+    requirementObject.forEach(reqObj => {
+        //create full url as per reqObj
+        console.log('creating url');
+        let dataForUrl = `district_id=${reqObj.district_id}&date=${moment().format('DD-MM-YYYY')}`;
+        const finalUrl = `${calendarByDistrictUrl}${dataForUrl}`;
+
+        console.log(reqObj.name);
+
+        https.get(finalUrl, (resp) => {
+            let data = '';
+            
+            // A chunk of data has been received.
+            resp.on('data', (chunk) => {
+              data += chunk;
+            });
         
-        // A chunk of data has been received.
-        resp.on('data', (chunk) => {
-          data += chunk;
-        });
-    
-        // The whole response has been received. Print out the result.
-        resp.on('end', () => {
-          console.log('End');
-          console.log(data);
-        });
-      }).on("error", (err) => {
-        console.log("Error: " + err.message);
-      });
+            // The whole response has been received. Print out the result.
+            resp.on('end', () => {
+              console.log('End');
+              console.log(data);
+            });
+          }).on("error", (err) => {
+            console.log("Error: " + err.message);
+          });
+    });
 }
 
-module.exports = ({cronjobFunc, apiCall});
+module.exports = ({cronjobFunc, apiCalls});
